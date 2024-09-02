1a4d033169c6b8c2792cf3b8a8cb595150a3ad39
Khoirusy Syafaat <syafaatkhoirusy@gmail.com>
2024-09-02 12:32:45 +0700

    update tambahan shortcut ekualisasi tahunan pada menu ekualisasi group

--- app/Admin/Controllers/EkualisasiGroupController.php
+++ app/Admin/Controllers/EkualisasiGroupController.php
@@ -6,6 +6,7 @@ use OpenAdmin\Admin\Controllers\AdminController;
 use OpenAdmin\Admin\Grid;
 use App\Models\Client;
 use App\Models\Ekualisasigroup;
+use \App\Models\Ekualisasitahunan;
 use App\Models\Ekualisasi;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Pagination\LengthAwarePaginator;
@@ -13,7 +14,6 @@ use Illuminate\Support\Collection;
 use OpenAdmin\Admin\Form;
 use OpenAdmin\Admin\Show;
 use Illuminate\Http\Request;
-
 use OpenAdmin\Admin\Widgets\Table;
 
 
@@ -34,7 +34,7 @@ class EkualisasiGroupController extends AdminController
     protected function grid()
 {
     $grid = new Grid(new Ekualisasigroup());
-
+   
     // Define the columns for the grid
     $grid->column('id', 'Detail')->expand(function ($model) {
         // Mengambil semua data pemeriksaan yang terkait dengan client_id ini
@@ -83,6 +83,63 @@ class EkualisasiGroupController extends AdminController
         // Menampilkan data dalam tabel dengan kolom yang telah ditentukan
         return new Table(['Nama WP', 'Masa Pajak', 'Tanggal Masa Pajak', 'Diperiksa Oleh', 'Mengetahui', 'Status', 'Action'], $data->toArray());
     });
+
+    $grid->column('id_tahunan', 'Ekualisasi Tahunan')->expand(function ($model) {        
+        $ekualisasiTahunan = Ekualisasitahunan::where('id', $model->id_tahunan)->first();
+
+        // Check if the instance exists
+        if ($ekualisasiTahunan) {
+            // Use the instance to get the related details
+            $details = $ekualisasiTahunan->ekualisasiDetails;
+        } else {
+            $details = collect(); // Return an empty collection if no instance is found
+        }
+        //dd($details);
+        // Function to process details and calculate quantities
+        $processDetails = function ($details, &$quantity, &$jumlah, &$dpp, &$dppg, &$ppn, &$tipeppn) {
+            return $details->map(function ($detail) use (&$quantity, &$jumlah, &$dpp, &$dppg, &$ppn, &$tipeppn) {
+                $itemName = $detail->item_ekualisasi->item_pemeriksaan ?? 'Unknown';
+                $quantity = $detail->quantity;
+                //$jumlah = $detail->jumlah;
+                $dpp = $detail->dpp_faktur_pajak;
+                $dppg = $detail->dpp_gunggung;
+                $ppn = $detail->ppn_pph;
+                $jumlah = $dpp + $dppg;
+                $tipeppn = $detail->item_ekualisasi->tipe_ppn_pph ?? 'Unknown';
+
+                if($tipeppn == 0):
+                    $tipe = "<span class='badge bg-warning'>PPn</badge>";
+                    $ppn = $detail->ppn_pph;
+                    $pph = 0;
+                elseif($tipeppn == 1):
+                    $tipe = "<span class='badge bg-success'>PPh</badge>";
+                    $pph = $detail->ppn_pph;
+                    $ppn = 0;
+                else:
+                    $tipe = "<span class='badge bg-danger'>-</badge>";
+                    $ppn = 0;
+                    $pph = 0;
+                endif;
+    
+                return [
+                    'ID' => $detail->item_pemeriksaan_id,
+                    'item_pemeriksaan' => $itemName.' '.$tipe,
+                    'quantity' => number_format($detail->quantity, 0, ",", "."),
+                    'dpp_faktur_pajak' => number_format($detail->dpp_faktur_pajak, 0, ",", "."),
+                    'dpp_gunggung' => number_format($detail->dpp_gunggung, 0, ",", "."),
+                    'jumlah' => number_format($jumlah, 0, ",", "."),
+                    'ppn' => number_format($ppn, 0, ",", "."),
+                    'pph' => number_format($pph, 0, ",", "."),
+                    'keterangan' => $detail->keterangan,
+                    // 'created_at' => $detail->created_at,
+                ];
+            });
+        };
+    
+        $data = $processDetails($details, $quantity, $dpp, $dppg,$jumlah, $ppn, $ppn);
+
+        return new Table(['ID', 'Item Ekualisasi', 'quantity', 'dpp faktur pajak', 'dpp gungung','jumlah', 'PPn','PPh', 'Ket'], $data->toArray());
+    });
     
     
     
@@ -110,6 +167,14 @@ class EkualisasiGroupController extends AdminController
             $filter->equal('client_id')->select(Client::all()->pluck('nama_wp', 'id'));
         });
         
+    });
+    
+    $grid->column('Detail')->display(function () {
+        // Retrieve the id_tahunan for the URL
+        $url = $this->id_tahunan;
+    
+        // Customize the edit button link with a detail URL
+        return "<a href='tahunan-detail?id={$url}' target='_blank' class='btn btn-xs btn-primary'>Detail Ekualisasi Tahunan</a>";
     });
 
     // Disable create button
